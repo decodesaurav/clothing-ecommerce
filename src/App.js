2dd59f657ed070eaf32b1f54503910fd08dd2eb1
decodesaurav <decodesaurav@gmail.com>
2022-11-21 10:19:09 +0545

    In the latest commit we've added the header file and just started creating our sign-in and sign-up pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import HomePage from "./pages/Homepage/homepage.component";
 import {  Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/Shop-page/shop.component";
-
+import Header from "./Components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page-component";
  
 function App() {
   return(
     <div>
+      <Header />
        <Switch>        
           <Route exact path= '/' component= { HomePage } /> 
           <Route path= '/shop' component= { ShopPage } />
+          <Route path= '/signin' component = { SignInAndSignUpPage } />
       </Switch> 
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page-component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './sign-in-and-sign-up-page-styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+        SIGN IN
+    </div>
+);
+
+export default SignInAndSignUpPage;
